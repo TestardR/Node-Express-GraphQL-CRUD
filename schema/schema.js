83a3c3e58ccc4da68aed190a40ecc51acc3599ab
rtestard <rtestard@opensense.fr>
2019-11-22 16:14:19 +0100

    first commit

---
+++ schema/schema.js
@@ -0,0 +1,46 @@
+const graphql = require("graphql");
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+const _ = require("lodash");
+const axios = require("axios");
+const url = "http://localhost:3000/";
+
+const CompanyType = new GraphQLObjectType({
+  name: "Company",
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString }
+  }
+});
+
+const UserType = new GraphQLObjectType({
+  name: "User",
+  fields: {
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt },
+    company: {
+        type: CompanyType,
+        resolve(parentValue, args) {
+           return axios.get(`${url}companies/${parentValue.companyId}`).then(res => res.data)
+        }
+    }
+  }
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return axios.get(`${url}users/${args.id}`).then(res => res.data);
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
